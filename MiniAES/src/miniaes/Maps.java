11e2b3534a15810674ddb62146795871db6a50c8
1812962 <1812962@RG-N527-D03.rgu.ac.uk>
2019-03-28 10:13:47 +0000

    Setup classes as advised by Roger.

---
+++ MiniAES/src/miniaes/Maps.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniaes;
+
+/**
+ *
+ * @author 1812962
+ */
+public class Maps {
+    
+}
