3fbad50bbe0ae0ccc434331eedef0709a19d6293
FergusMather <43371099+FergusMather@users.noreply.github.com>
2019-03-28 17:32:41 +0000

    Fixed plaintext initialise by setting array length to 2.

--- MiniAES/src/miniaes/Encryptor.java
+++ MiniAES/src/miniaes/Encryptor.java
@@ -12,41 +12,27 @@ package miniaes;
 public class Encryptor {
     //Setup fields two 2D arrays for the 16 bit blocks of 4bit
     String [][] key;
-    String [][] plainText = new String[1][1];
-    
-    
-    //for(int i=0;i<2;i++){
-            //for(int j=0;j<2;j++){
-               // plainText[i][j] = "0000";
-            //}
-        //}
-    
-    Encryptor(String bitString){
+    String [][] plainText = new String[2][2];  
         
+    Encryptor(String bitString){
         int l = 0;
         for(int i =0;i<plainText.length;i++){
             for(int j=0;j<plainText[i].length;j++){
                 String temp = "";
                 for(int k=0;k<4;k++){                    
                     temp += bitString.charAt(l);
-                    l++;
+                    l++;                    
                 }
-                plainText[i][j] = temp;
+                plainText[i][j] = temp;                
             }
         }
-    }
-    
-    public String getTest(int a, int b){
-        return plainText[a][b];
-    }
+    }   
     
     public String outputArray(){
         String output = "";
         for(int i=0;i<plainText.length;i++){
-            for(int j=0;j<plainText[i].length;j++){
-                for(int k=0;k<4;k++){
-                  output += plainText[i][j].charAt(k);  
-                }
+            for(int j=0;j<plainText[i].length;j++){                
+                output += plainText[i][j];                
                 output += "\t";
             }
             output += "\n";
