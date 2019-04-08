94fed7b427f389110aa85baa25cf55a1242b96ca
1812962 <1812962@RG-N533-C01.rgu.ac.uk>
2019-04-08 16:43:18 +0100

    created framework for plan

---
+++ MiniAES/src/miniaes/Key.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniaes;
+
+/**
+ *
+ * @author 1812962
+ */
+public class Key {    
+    String [][] keyZero = new String [2][2];
+    String [][] keyOne = new String [2][2];
+    String [][] keyTwo = new String [2][2];    
+    Calculator calculator = new Calculator();
+    
+    Key(String userKey){        
+        //generate key0 key1 and key2 from initial string
+    }    
+    
+    public String[][] getKey(int num){
+        //return key according to num
+        String[][] temp = new String[1][1];
+        temp[0][0] = "Hello";
+        return temp;
+    }
+}
